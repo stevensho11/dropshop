4acd2b7cd5ecf9e07dfe4ecfbc059667d7385add
Stevens  Ho <stevensho@stevens-mbp.fios-router.home>
2023-11-04 01:49:20 -0400

    adding redux for state management

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,29 +1,35 @@
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../Components/Product";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
-
+  const { data: products, isLoading, error } = useGetProductsQuery();
+  console.log(products);
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col className="my-3" key={product.id} sm={12} mg={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col
+                key={product.id}
+                className="my-3"
+                sm={12}
+                mg={6}
+                lg={4}
+                xl={3}
+              >
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
