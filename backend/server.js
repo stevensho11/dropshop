c3aaaa00e88a8dce274748ae5d5b6c011167e5fe
Stevens Ho <stevensho@stevensho.com>
2023-11-05 23:52:44 -0500

    added jwt and backend routes

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); //connect MongoDB
 
 const app = express();
+
+//Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('api running');
 })
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -5,7 +5,6 @@ import { useSelector } from "react-redux";
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
-  console.log(cartItems);
 
   return (
     <header>
